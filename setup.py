3b5c2a23e97f7c29b11b008ab858e70b0c285531
Stephen Crawley <s.crawley@uq.edu.au>
2021-09-10 11:00:41 +1000

    Switch to pbr style setup + requirements
    If we don't do this, setting up the unit tests gets gnarly
    
    Change-Id: Ief47bc21e4ca3f11bc5e8129ab814e765f48056d

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
-
-from pbr.packaging import parse_requirements
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='bumblebee',
-    version='0.1',
-    description=('Virtual Desktop service'),
-    author='ARDC',
-    author_email='coreservices@ardc.edu.au',
-    url='https://github.com/NeCTAR-RC/bumblebee',
-    license='Apache License 2.0',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=parse_requirements(),
-    classifiers=(
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ),
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
+
+# from pbr.packaging import parse_requirements
+# from setuptools import setup, find_packages
+
+# setup(
+#     name='bumblebee',
+#     version='0.1',
+#     description=('Virtual Desktop service'),
+#     author='ARDC',
+#     author_email='coreservices@ardc.edu.au',
+#     url='https://github.com/NeCTAR-RC/bumblebee',
+#     license='Apache License 2.0',
+#     packages=find_packages(),
+#     include_package_data=True,
+#     zip_safe=False,
+#     install_requires=parse_requirements(),
+#     classifiers=(
+#         'Development Status :: 2 - Pre-Alpha',
+#         'Intended Audience :: Developers',
+#         'License :: OSI Approved :: Apache Software License',
+#         'Natural Language :: English',
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3.6',
+#         'Programming Language :: Python :: 3.7',
+#         'Programming Language :: Python :: 3.8',
+#         'Programming Language :: Python :: 3.9',
+#     ),
+# )
