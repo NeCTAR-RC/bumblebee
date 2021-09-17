777af72093b559d73973e65d046145ac90cfe611
Andy Botting <andy@andybotting.com>
2021-09-17 11:22:45 +1000

    Extend DesktopType
    Change-Id: Ib93e3af9ef12ad66fac53a4c20e68f9f467c49e9

--- researcher_desktop/admin.py
+++ researcher_desktop/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from researcher_desktop.models import DesktopType
+
+@admin.register(DesktopType)
+class DesktopTypeAdmin(admin.ModelAdmin):
+    pass
