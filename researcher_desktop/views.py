606f35c268a00d14ca0483df1fe4c487d43639da
Stephen Crawley <s.crawley@uq.edu.au>
2021-09-20 12:40:48 +1000

    WIP - Replacing the clunky VM_Info and associated hash
     - The DesktopType class now has methods for getting the flavors
       and the desktop volume from the respective names.
     - The vm_manager code now uses DesktopType instances rather
       than hashes
     - So far ... launch and connect are working (again)
    
    Change-Id: I6b4377099824511cfc2af4e10b95267750930fab

--- researcher_desktop/views.py
+++ researcher_desktop/views.py
@@ -8,8 +8,10 @@ from django.urls import reverse
 from researcher_workspace.models import Feature
 from researcher_desktop.constants import NOTIFY_VM_PATH_PLACEHOLDER
 from researcher_desktop.utils.user_data_ubuntu import user_data_ubuntu
-from researcher_desktop.utils.utils import get_vm_info
-from researcher_desktop.utils.utils import get_desktop_types
+from researcher_desktop.utils.utils import desktop_types
+from researcher_desktop.utils.utils import get_desktop_type
+from researcher_desktop.utils.utils import desktops_feature
+
 from vm_manager import views as vm_man_views
 from researcher_workspace.utils import not_support_staff, redirect_home
 from vm_manager.constants import LINUX, REBOOT_BUTTON, SHELVE_BUTTON, DELETE_BUTTON, BOOST_BUTTON
@@ -18,13 +20,11 @@ logger = logging.getLogger(__name__)
 
 
 def render_modules(request):
-    catalog = get_vm_info()
     feature_modules = []
     feature_scripts = []
-    for desktop_type in get_desktop_types():
+    for desktop_type in desktop_types():
         feature_module, feature_script = vm_man_views.render_vm(
             request, request.user, desktop_type,
-            catalog.FEATURE,
             [REBOOT_BUTTON, SHELVE_BUTTON, DELETE_BUTTON, BOOST_BUTTON])
         feature_modules.append(feature_module)
         feature_scripts.append(feature_script)
@@ -33,93 +33,64 @@ def render_modules(request):
 @login_required(login_url='login')
 @user_passes_test(test_func=not_support_staff, login_url='staff_home', redirect_field_name=None)  # Only need to stop support staff creating vms, as they can't use any other function if they don't have a vm
 def launch_vm(request, desktop):
-    desktop_type_ids = [d.id for d in get_desktop_types()]
-    if desktop not in desktop_type_ids:
-        logger.error(f"Unrecognised desktop ({desktop}) was requested by {request.user}")
-        raise Http404
-
-    vm_info = {}
-    catalog = get_vm_info()
-    vm_info['flavor'] = catalog.DESKTOPS[desktop]['default_flavor']
-    vm_info['source_volume'] = catalog.DESKTOPS[desktop]['source_volume']
-    vm_info['operating_system'] = desktop
-    vm_info['user_data_script'] = user_data_ubuntu\
-                .replace(NOTIFY_VM_PATH_PLACEHOLDER,
-                         reverse('researcher_desktop:notify_vm'))
-    vm_info['security_groups'] = settings.OS_SECGROUPS
+    desktop_type = get_desktop_type(desktop)
+    print("desktop type %s" % desktop_type)
     logger.info(request.user) 
-    vm_man_views.launch_vm(request.user, vm_info, catalog.FEATURE)
+    vm_man_views.launch_vm(request.user, desktop_type)
     return redirect_home(request)
 
 
 @login_required(login_url='login')
 def delete_vm(request, vm_id):
-    catalog = get_vm_info()
-    vm_man_views.delete_vm(request.user, vm_id, catalog.FEATURE)
+    vm_man_views.delete_vm(request.user, vm_id, desktops_feature())
     return redirect_home(request)
 
 
 @login_required(login_url='login')
 def shelve_vm(request, vm_id):
-    catalog = get_vm_info()
-    vm_man_views.shelve_vm(request.user, vm_id, catalog.FEATURE)
+    vm_man_views.shelve_vm(request.user, vm_id, desktops_feature())
     return redirect_home(request)
 
 
 @login_required(login_url='login')
 def unshelve_vm(request, desktop):
-    if desktop not in desktop_type_ids():
-        logger.error(f"Unrecognised desktop ({desktop}) was requested by {request.user}")
-        raise Http404
-
-    vm_info = {}
-    catalog = get_vm_info()
-    vm_info['flavor'] = catalog.DESKTOPS[desktop]['default_flavor']
-    vm_info['source_volume'] = catalog.DESKTOPS[desktop]['source_volume']
-    vm_info['operating_system'] = desktop
-    vm_info['user_data_script'] = ""
-    vm_info['security_groups'] = settings.OS_SECGROUPS
-    vm_man_views.unshelve_vm(request.user, vm_info, catalog.FEATURE)
+    desktop_type = get_desktop_type(desktop)
+    vm_man_views.unshelve_vm(request.user, vm_info, desktop_type)
     return redirect_home(request)
 
 
 @login_required(login_url='login')
 def reboot_vm(request, vm_id, reboot_level):
-    catalog = get_vm_info()
-    vm_man_views.reboot_vm(request.user, vm_id, reboot_level, catalog.FEATURE)
+    vm_man_views.reboot_vm(request.user, vm_id,
+                           reboot_level, desktops_feature())
     return redirect_home(request)
 
 
 @login_required(login_url='login')
 def get_rdp_file(request, vm_id):
-    catalog = get_vm_info()
-    rdp_file = vm_man_views.get_rdp_file(request.user, vm_id, catalog.FEATURE)
+    rdp_file = vm_man_views.get_rdp_file(request.user, vm_id,
+                                         desktops_feature())
     response = HttpResponse(rdp_file, content_type='text/plain')
-    response['Content-Disposition'] = 'attachment; filename="research_desktop.rdp"'
+    response['Content-Disposition'] = \
+        'attachment; filename="research_desktop.rdp"'
     return response
 
 
 @login_required(login_url='login')
 def supersize_vm(request, vm_id):
-    catalog = get_vm_info()
-    # FIX ME: need to know what the current desktop type is
-    vm_man_views.supersize_vm(request.user, vm_id, catalog.BIG_FLAVOR,
-                              catalog.FEATURE)
+    vm_man_views.supersize_vm(request.user, vm_id, desktops_feature())
     return redirect_home(request)
 
 
 @login_required(login_url='login')
 def downsize_vm(request, vm_id):
-    catalog = get_vm_info()
-    # FIX ME: need to know what the current desktop type is(?)
-    vm_man_views.downsize_vm(request.user, vm_id, catalog.FEATURE)
+    vm_man_views.downsize_vm(request.user, vm_id, desktops_feature())
     return redirect_home(request)
 
 
 @login_required(login_url='login')
 def extend(request, vm_id):
-    catalog = get_vm_info()
-    vm_man_views.extend(request.user, vm_id, catalog.FEATURE)
+    vm_man_views.extend(request.user, vm_id, desktops_feature())
     return redirect_home(request)
 
 
@@ -127,28 +98,25 @@ def extend(request, vm_id):
 def start_downsizing_cron_job(request):
     if not request.user.is_superuser:
         raise Http404()
-    catalog = get_vm_info()
     return HttpResponse(
-        vm_man_views.start_downsizing_cron_job(catalog.FEATURE),
+        vm_man_views.start_downsizing_cron_job(desktops_feature()),
         content_type='text/plain')
 
 
 def notify_vm(request):
-    catalog = get_vm_info()
-    return HttpResponse(vm_man_views.notify_vm(request, catalog.FEATURE))
+    return HttpResponse(vm_man_views.notify_vm(request, desktops_feature()))
 
 
 @login_required(login_url='login')
 def status_vm(request, desktop):
-    catalog = get_vm_info()
-    state, what_to_show, vm_id = vm_man_views.get_vm_state(request.user, desktop, catalog.FEATURE)
+    state, what_to_show, vm_id = vm_man_views.get_vm_state(
+        request.user, get_desktop_type(desktop))
     result = {'state': state, 'status': what_to_show}
     return JsonResponse(result)
 
 
 def rd_report(reporting_months):
-    return vm_man_views.vm_report_for_csv(reporting_months,
-                                          desktop_type_ids())
+    return vm_man_views.vm_report_for_csv(reporting_months, desktop_types())
 
 
 def rd_report_page():
@@ -161,6 +129,5 @@ def rd_report_page():
 
 
 def rd_report_for_user(user):
-    catalog = get_vm_info()
-    return vm_man_views.rd_report_for_user(user, desktop_type_names(),
-                                           catalog.FEATURE)
+    return vm_man_views.rd_report_for_user(user, desktop_types(),
+                                           desktops_feature())
