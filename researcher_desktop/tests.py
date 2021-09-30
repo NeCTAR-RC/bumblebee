7cf4acff9dc719967d77eaa0b6329972758dc443
Stephen Crawley <s.crawley@uq.edu.au>
2021-09-30 12:26:46 +1000

    Unit tests / fixes for researcher_desktop utils
    Change-Id: I3caa45e2be6adcdd95ff0e0c94c51dabea2a27c9

--- researcher_desktop/tests.py
+++ researcher_desktop/tests.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
-from researcher_desktop.utils.utils import desktop_type_ids
+
+from django.http import Http404
+
+from researcher_desktop.utils.utils import desktop_types, get_desktop_type, \
+    desktops_feature
 
 
 class UtilsTests(TestCase):
 
-    def test_desktop_type_ids(self):
+    def test_desktop_types(self):
         # Assuming that the types are as populated by the 'bootstrap'
         # migration.  This may need to change ...
-        ids = desktop_type_ids()
-        self.assertEqual(2, len(ids))
-        self.assertEqual('ubuntu', ids[0])
-        self.assertEqual('centos', ids[1])
+        types = list(desktop_types())
+        self.assertEqual(2, len(types))
+        self.assertEqual('ubuntu', types[0].id)
+        self.assertEqual('centos', types[1].id)
 
+    def test_get_desktop_type(self):
+        desktop_type = get_desktop_type('ubuntu')
+        self.assertEqual('ubuntu', desktop_type.id)
+        self.assertRaises(Http404, get_desktop_type, 'unknown')
+
+    def test_desktops_feature(self):
+        self.assertIsNotNone(desktops_feature())
--- researcher_desktop/utils/utils.py
+++ researcher_desktop/utils/utils.py
@@ -1,21 +1,28 @@
+import logging
+
+from django.http import Http404
+
 from researcher_desktop.constants import APP_NAME
 from researcher_desktop.models import DesktopType
 from researcher_workspace.models import Feature
 from vm_manager.utils.utils import get_nectar
 
 
+logger = logging.getLogger(__name__)
+
+
 def desktops_feature():
     return Feature.objects.get(app_name=APP_NAME)
 
 
-def get_desktop_type(id, enabled=True):
+def get_desktop_type(id, only_enabled=True):
     try:
         desktop_type = DesktopType.objects.get(id=id)
-        if enabled and not desktop_type.enabled:
+        if only_enabled and not desktop_type.enabled:
             logger.error(f"Disabled desktop({id}) requested")
             raise Http404
         return desktop_type
-    except DesktopType.NotFound:
+    except DesktopType.DoesNotExist:
         logger.error(f"Unknown desktop({id}) requested")
         raise Http404
 
