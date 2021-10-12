ac9bf354a7e01ff679dbaf3d4d1ba10fe6e10928
Stephen Crawley <s.crawley@uq.edu.au>
2021-10-12 11:36:17 +1100

    Yet more vm_manager.views tests
      - render_vm, notify_vm
    
    Change-Id: Id015e5063463a6dff2bd2c6fadabd3040cc6cb3b

--- vm_manager/tests/unit/test_views.py
+++ vm_manager/tests/unit/test_views.py
@@ -2,7 +2,7 @@ import uuid
 import copy
 from datetime import datetime, timedelta, timezone
 
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, call
 
 from django.http import Http404
 from django.test import TestCase
@@ -19,15 +19,15 @@ from vm_manager.constants import ERROR, ACTIVE, SHUTDOWN, VERIFY_RESIZE, \
 from vm_manager.constants import VM_OKAY, VM_DELETED, VM_WAITING, \
     VM_CREATING, VM_RESIZING, NO_VM, VM_SHELVED, VM_MISSING, VM_ERROR, \
     VM_SHUTDOWN, VM_SUPERSIZED, ALL_VM_STATES, LAUNCH_WAIT_SECONDS, \
-    DOWNSIZE_PERIOD
-from vm_manager.models import VMStatus, Instance
+    DOWNSIZE_PERIOD, CLOUD_INIT_STARTED, CLOUD_INIT_FINISHED, SCRIPT_OKAY
+from vm_manager.models import VMStatus, Instance, Volume
 from vm_manager.utils.utils import get_nectar, after_time
 from vm_manager.views import launch_vm_worker, delete_vm_worker, \
-    shelve_vm_worker, unshelve_vm_worker, reboot_vm_worker, supersize_vm_worker, \
-    downsize_vm_worker
+    shelve_vm_worker, unshelve_vm_worker, reboot_vm_worker, \
+    supersize_vm_worker, downsize_vm_worker
 
 from vm_manager.views import launch_vm, delete_vm, shelve_vm, unshelve_vm, \
-    reboot_vm, supersize_vm, downsize_vm, get_vm_state
+    reboot_vm, supersize_vm, downsize_vm, get_vm_state, render_vm, notify_vm
 
 
 class VMManagerViewTests(TestCase):
@@ -44,6 +44,7 @@ class VMManagerViewTests(TestCase):
     def build_existing_vm(self, status):
         self.volume = VolumeFactory.create(
             id=uuid.uuid4(), user=self.user,
+            operating_system=self.desktop_type.id,
             requesting_feature=self.feature)
         self.instance = InstanceFactory.create(
             id=uuid.uuid4(), user=self.user, boot_volume=self.volume,
@@ -446,7 +447,7 @@ class VMManagerViewTests(TestCase):
         time1 = (datetime.now(timezone.utc)
                  + timedelta(days=DOWNSIZE_PERIOD)).date()
         time2 = (datetime.now(timezone.utc)
-                 + timedelta(days=(DOWNSIZE_PERIOD * 2))).date()
+                 + timedelta(days=DOWNSIZE_PERIOD * 2)).date()
         resize = ResizeFactory.create(instance=self.instance, expires=time1)
         self.assertEqual((VM_SUPERSIZED,
                           {
@@ -457,3 +458,177 @@ class VMManagerViewTests(TestCase):
                           },
                           self.instance.id),
                          get_vm_state(self.user, self.desktop_type))
+
+    @patch('vm_manager.views.loader')
+    @patch('vm_manager.models.Instance.get_url')
+    @patch('vm_manager.views.messages')
+    def test_render_vm(self, mock_messages, mock_get_url, mock_loader):
+        url = "https://foo/bar"
+        mock_get_url.return_value = url
+
+        # Not testing what is actually rendered.
+        mock_loader.render_to_string.return_value = "rendered"
+
+        request = "The Request"
+        buttons = ["ONE", "TWO"]
+
+        self.build_existing_vm(VM_OKAY)
+        self.assertEqual(("rendered", "rendered"),
+                         render_vm(request, self.user, self.desktop_type,
+                                   buttons))
+        context = {
+            'what_to_show': {'url': url},
+            'desktop_type': self.desktop_type,
+            'vm_id': self.instance.id,
+            'buttons_to_display': buttons,
+            'app_name': self.feature.app_name,
+            'requesting_feature': self.feature,
+            'VM_WAITING': VM_WAITING}
+        calls = [call(f"vm_manager/html/{VM_OKAY}.html",
+                      context, request),
+                 call(f"vm_manager/javascript/{VM_OKAY}.js",
+                      context, request)]
+        mock_loader.render_to_string.assert_has_calls(calls)
+        mock_loader.render_to_string.reset_mock()
+        mock_messages.info.assert_not_called()
+
+        # Reset for supersized (non extendable)
+        self.build_existing_vm(VM_SUPERSIZED)
+        time1 = (datetime.now(timezone.utc)
+                 + timedelta(days=DOWNSIZE_PERIOD)).date()
+        time2 = (datetime.now(timezone.utc)
+                 + timedelta(days=DOWNSIZE_PERIOD * 2)).date()
+        resize = ResizeFactory.create(instance=self.instance, expires=time1)
+
+        self.assertEqual(("rendered", "rendered"),
+                         render_vm(request, self.user, self.desktop_type,
+                                   buttons))
+        context['what_to_show'] = {
+            'url': url,
+            'is_eligible': False,
+            'expires': time1,
+            'extended_expiration': time2
+        }
+        context['vm_id'] = self.instance.id
+        calls = [call(f"vm_manager/html/{VM_SUPERSIZED}.html",
+                      context, request),
+                 call(f"vm_manager/javascript/{VM_SUPERSIZED}.js",
+                      context, request)]
+        mock_loader.render_to_string.assert_has_calls(calls)
+        mock_messages.info.assert_not_called()
+
+        # Reset for supersized (extendable)
+        self.build_existing_vm(VM_SUPERSIZED)
+        time1 = (datetime.now(timezone.utc)
+                 + timedelta(days=DOWNSIZE_PERIOD - 1)).date()
+        time2 = (datetime.now(timezone.utc)
+                 + timedelta(days=DOWNSIZE_PERIOD * 2 - 1)).date()
+        resize = ResizeFactory.create(instance=self.instance, expires=time1)
+
+        self.assertEqual(("rendered", "rendered"),
+                         render_vm(request, self.user, self.desktop_type,
+                                   buttons))
+        context['what_to_show'] = {
+            'url': url,
+            'is_eligible': True,
+            'expires': time1,
+            'extended_expiration': time2
+        }
+        context['vm_id'] = self.instance.id
+
+        mock_loader.render_to_string.assert_has_calls(calls)
+        mock_messages.info.assert_called_with(
+            request,
+            f'Your {str(self.desktop_type).capitalize()} vm is set to resize '
+            f'back to the default size on {time1}')
+
+    @patch('vm_manager.views.logger')
+    @patch('vm_manager.views.generate_hostname')
+    def test_notify_vm_inconsistent(self, mock_gen, mock_logger):
+        fake_request = Fake(GET={
+            'ip': '10.0.0.1',
+            'hn': 'foo',
+            'os': self.desktop_type.id,
+            'state': SCRIPT_OKAY,
+            'msg': CLOUD_INIT_STARTED
+        })
+        with self.assertRaises(Http404):
+            notify_vm(fake_request, self.feature)
+        mock_logger.error.assert_called_with(
+            "No current Instance found with ip address 10.0.0.1")
+
+        mock_gen.return_value = "bzzzt"
+        self.build_existing_vm(VM_WAITING)
+        with self.assertRaises(Http404):
+            notify_vm(fake_request, self.feature)
+        mock_logger.error.assert_called_with(
+            f"Hostname provided in request does not match "
+            f"hostname of volume {self.instance}, foo")
+        mock_gen.assert_called_with(
+            self.volume.hostname_id, self.desktop_type.id)
+
+        mock_gen.reset_mock()
+        mock_logger.error.reset_mock()
+        mock_gen.return_value = "foo"
+
+        self.assertEqual(
+            f"{self.instance.ip_address}, {self.desktop_type.id}, "
+            f"{SCRIPT_OKAY}, {CLOUD_INIT_STARTED}",
+            notify_vm(fake_request, self.feature))
+
+        mock_gen.assert_called_with(
+            self.volume.hostname_id, self.desktop_type.id)
+        mock_logger.error.assert_not_called()
+        volume = Volume.objects.get(pk=self.volume.pk)
+        vm_status = VMStatus.objects.get(pk=self.vm_status.pk)
+        self.assertTrue(volume.checked_in)
+        self.assertFalse(volume.ready)
+        self.assertEqual(VM_WAITING, vm_status.status)
+
+        mock_gen.reset_mock()
+        mock_logger.error.reset_mock()
+        fake_request = Fake(GET={
+            'ip': '10.0.0.1',
+            'hn': 'foo',
+            'os': self.desktop_type.id,
+            'state': SCRIPT_OKAY,
+            'msg': CLOUD_INIT_FINISHED
+        })
+
+        self.assertEqual(
+            f"{self.instance.ip_address}, {self.desktop_type.id}, "
+            f"{SCRIPT_OKAY}, {CLOUD_INIT_FINISHED}",
+            notify_vm(fake_request, self.feature))
+
+        mock_gen.assert_called_with(
+            self.volume.hostname_id, self.desktop_type.id)
+        mock_logger.error.assert_not_called()
+        volume = Volume.objects.get(pk=self.volume.pk)
+        vm_status = VMStatus.objects.get(pk=self.vm_status.pk)
+        self.assertTrue(volume.checked_in)
+        self.assertTrue(volume.ready)
+        self.assertEqual(VM_OKAY, vm_status.status)
+
+        mock_gen.reset_mock()
+        mock_logger.error.reset_mock()
+        fake_request = Fake(GET={
+            'ip': '10.0.0.1',
+            'hn': 'foo',
+            'os': self.desktop_type.id,
+            'state': 42,
+            'msg': "Other Message"
+        })
+
+        self.assertEqual(
+            f"{self.instance.ip_address}, {self.desktop_type.id}, "
+            f"42, Other Message",
+            notify_vm(fake_request, self.feature))
+
+        mock_gen.assert_called_with(
+            self.volume.hostname_id, self.desktop_type.id)
+        mock_logger.error.assert_called_with(
+            f"notify_vm error: Other Message for instance: "
+            f"\"{self.instance}\"")
+        vm_status = VMStatus.objects.get(pk=self.vm_status.pk)
+        self.assertEqual(VM_ERROR, vm_status.status)
+        self.assertEqual("Other Message", vm_status.instance.error_message)
