361a4f07530f6c034fde528e71c4558647541eee
Stephen Crawley <s.crawley@uq.edu.au>
2021-10-14 10:53:09 +1100

    Unit tests for vm_manager.vm_functions.delete_vm
      - worker_vm_delete
      - factored out a base testclass
    
    Change-Id: Ib25e8141f721263f68bb3731d8ad7625237587f5

---
+++ vm_manager/tests/unit/vm_functions/base.py
@@ -0,0 +1,59 @@
+import uuid
+
+from django.test import TestCase
+
+from researcher_workspace.tests.factories import UserFactory
+from researcher_desktop.utils.utils import get_desktop_type, desktops_feature
+
+from vm_manager.tests.common import UUID_1, UUID_2, UUID_3, UUID_4
+from vm_manager.tests.fakes import Fake, FakeServer, FakeVolume, FakeNectar
+from vm_manager.tests.factories import InstanceFactory, VMStatusFactory, \
+    VolumeFactory
+
+from vm_manager.constants import VM_MISSING, VM_OKAY, VM_SHELVED, NO_VM, \
+    VOLUME_CREATION_TIMEOUT
+
+
+class VMFunctionTestBase(TestCase):
+
+    def setUp(self, *args, **kwargs):
+        super().setUp(*args, **kwargs)
+        self.FEATURE = desktops_feature()
+        self.UBUNTU = get_desktop_type('ubuntu')
+        self.CENTOS = get_desktop_type('centos')
+        self.user = UserFactory.create()
+
+    def build_fake_volume(self, id=None):
+        if id is None:
+            id = UUID_3
+        return VolumeFactory.create(
+            id=id,
+            user=self.user,
+            image=self.UBUNTU.source_volume_id,
+            operating_system=self.UBUNTU.id,
+            requesting_feature=self.UBUNTU.feature,
+            flavor=self.UBUNTU.default_flavor.id)
+
+    def build_fake_vol_instance(self, volume_id=None, instance_id=None,
+                                ip_address=None):
+        if instance_id is None:
+            instance_id = UUID_4
+        fake_volume = self.build_fake_volume(id=volume_id)
+        fake_instance = InstanceFactory.create(
+            id=instance_id,
+            boot_volume=fake_volume,
+            user=self.user,
+            ip_address=ip_address)
+        return fake_volume, fake_instance
+
+    def build_fake_vol_inst_status(self, volume_id=None, instance_id=None,
+                                   status=VM_OKAY):
+        fake_volume, fake_instance = self.build_fake_vol_instance(
+            volume_id=volume_id, instance_id=instance_id)
+        fake_vmstatus = VMStatusFactory.create(
+            user=self.user,
+            instance=fake_instance,
+            operating_system=self.UBUNTU.id,
+            requesting_feature=self.UBUNTU.feature,
+            status=status)
+        return fake_volume, fake_instance, fake_vmstatus
---
+++ vm_manager/tests/unit/vm_functions/test_delete_vm.py
@@ -0,0 +1,66 @@
+import copy
+from datetime import datetime, timedelta, timezone
+import uuid
+
+import novaclient
+
+from unittest.mock import Mock, patch, call
+
+from django.conf import settings
+from django.test import TestCase
+
+from researcher_desktop.utils.utils import get_desktop_type, desktops_feature
+
+from vm_manager.tests.common import UUID_1, UUID_2, UUID_3, UUID_4
+from vm_manager.tests.fakes import Fake, FakeServer, FakeVolume, FakeNectar
+from vm_manager.tests.factories import InstanceFactory, VMStatusFactory, \
+    VolumeFactory
+from vm_manager.tests.unit.vm_functions.base import VMFunctionTestBase
+
+from vm_manager.constants import VM_MISSING, VM_OKAY, VM_SHELVED, NO_VM, \
+    INSTANCE_CHECK_SHUTOFF_RETRY_WAIT_TIME, \
+    INSTANCE_CHECK_SHUTOFF_RETRY_COUNT, INSTANCE_DELETION_RETRY_COUNT
+from vm_manager.models import VMStatus, Volume
+from vm_manager.vm_functions.delete_vm import delete_vm_worker, \
+    _check_instance_is_shutoff_and_delete, \
+    _delete_volume_once_instance_is_deleted
+from vm_manager.utils.utils import get_nectar
+
+
+class DeleteVMTests(VMFunctionTestBase):
+
+    @patch('vm_manager.utils.utils.Nectar', new=FakeNectar)
+    @patch('vm_manager.vm_functions.delete_vm.django_rq')
+    @patch('vm_manager.vm_functions.delete_vm.logger')
+    def test_delete_vm_worker(self, mock_logger, mock_rq):
+        mock_scheduler = Mock()
+        mock_rq.get_scheduler.return_value = mock_scheduler
+        _, fake_instance = self.build_fake_vol_instance(ip_address='10.0.0.99')
+
+        fake_nectar = get_nectar()
+        fake_nectar.nova.servers.stop.side_effect = \
+            novaclient.exceptions.NotFound(code=42)  # the code is ignored
+
+        result = delete_vm_worker(fake_instance)
+
+        self.assertIsNone(result)
+        fake_nectar.nova.servers.stop.assert_called_once_with(fake_instance.id)
+        mock_rq.get_scheduler.assert_called_once_with('default')
+        mock_scheduler.enqueue_in.assert_called_once_with(
+            timedelta(seconds=INSTANCE_CHECK_SHUTOFF_RETRY_WAIT_TIME),
+            _check_instance_is_shutoff_and_delete,
+            fake_instance,
+            INSTANCE_CHECK_SHUTOFF_RETRY_COUNT,
+            _delete_volume_once_instance_is_deleted,
+            (fake_instance, INSTANCE_DELETION_RETRY_COUNT))
+
+        mock_logger.error.assert_called_once_with(
+            f"Trying to delete an instance that's missing "
+            f"from OpenStack {fake_instance}")
+        mock_logger.info.assert_has_calls([
+            call(f"About to delete vm at addr: 10.0.0.99 "
+                 f"for user {self.user.username}"),
+            call(f"Checking whether {fake_instance} is ShutOff "
+                 f"after {INSTANCE_CHECK_SHUTOFF_RETRY_WAIT_TIME} "
+                 f"seconds and Delete it")
+            ])
