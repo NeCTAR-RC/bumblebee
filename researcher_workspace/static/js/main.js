397bb3929a8615196c3499086f8bad89407aca71
Darcelle Malby <d.malby@uq.edu.au>
2021-10-27 08:20:47 +1100

    Added smooth scroll and now truncating workspace info on home page
    Change-Id: I7a5bd9a13077b97df1646c40fad92a2296291389

--- researcher_workspace/static/js/main.js
+++ researcher_workspace/static/js/main.js
@@ -10,3 +10,14 @@ var alertMessages = alertMessageList.map(function (toastEl) {
 window.onload = function() {
   alertMessages.forEach(toast => toast.show()); // Show any alert messages
 };
+
+// Add smooth scroll to anchors
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log(this.getAttribute('href'));
+      document.getElementById(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
