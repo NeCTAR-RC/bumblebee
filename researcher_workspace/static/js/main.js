b5741335ec8d44cba87ab92cae71103fd9ca911d
Darcelle Malby <d.malby@uq.edu.au>
2021-11-05 07:56:28 +1100

    Added logo images to pages and made some responsive design improvements
    Change-Id: I6f91a894a168c90beb0dff591146dd5e74eb4f46

--- researcher_workspace/static/js/main.js
+++ researcher_workspace/static/js/main.js
@@ -9,15 +9,20 @@ var alertMessages = alertMessageList.map(function (toastEl) {
 // Window ready event
 window.onload = function() {
   alertMessages.forEach(toast => toast.show()); // Show any alert messages
+  if(window.location.hash) { scrollToId(window.location.hash) };
 };
 
 // Add smooth scroll to anchors
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
       e.preventDefault();
-      console.log(this.getAttribute('href'));
-      document.getElementById(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+      scrollToId(this.getAttribute('href'));
   });
 });
+
+//Custom functions
+function scrollToId(elId) {
+  document.getElementById(elId).scrollIntoView({
+    behavior: 'smooth'
+  });
+}
