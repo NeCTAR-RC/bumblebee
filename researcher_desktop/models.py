62beefaf08ca19d5785acc05a7c712006f202d75
Andy Botting <andy@andybotting.com>
2021-11-04 14:10:33 +1100

    Change desktop type logo to a URL field
    Change-Id: I88b6e58cb5b1fe0bf36396c32103c444e46f11cf

--- researcher_desktop/models.py
+++ researcher_desktop/models.py
@@ -38,7 +38,7 @@ class DesktopType(models.Model):
     id = models.CharField(primary_key=True, max_length=32)
     name = models.CharField(max_length=128)
     description = models.TextField()
-    logo = models.ImageField(blank=True)
+    logo = models.URLField(blank=True, null=True)
     image_name = models.CharField(max_length=256)
     default_flavor_name = models.CharField(max_length=32)
     big_flavor_name = models.CharField(max_length=32)
