9f049d04f07ac78c390e3a96ef30a223299ce2b1
Darcelle Malby <d.malby@uq.edu.au>
2021-12-02 19:44:36 +1100

    Updated VM_Waiting.js for better progress bar functionality
      - and removed timer for now
    
    Change-Id: Icfdb32806dd127b080a2a8664f8a65343602145e

--- vm_manager/templates/vm_manager/javascript/VM_Waiting.js
+++ vm_manager/templates/vm_manager/javascript/VM_Waiting.js
@@ -1,33 +1,37 @@
 var {{ app_name }}_{{ desktop_type.id }}_timer = {{ what_to_show }};
-reloaded = false;
-var x = setInterval(function () {
-    {{ app_name }}_{{ desktop_type.id }}_timer = {{ app_name }}_{{ desktop_type.id }}_timer - 1;
-    if ({{ app_name }}_{{ desktop_type.id }}_timer <= 0 && !reloaded) {
-        reloaded = true;
-        window.location.reload(1);
-    }
-    const {{ app_name }}_{{ desktop_type.id }}_minutes = (Math.max(Math.floor({{ app_name }}_{{ desktop_type.id }}_timer/60), 0)).toString()
-    const {{ app_name }}_{{ desktop_type.id }}_seconds = (Math.max({{ app_name }}_{{ desktop_type.id }}_timer%60, 0)).toString().padStart(2, "0")
-    document.getElementById("{{ app_name }}-{{ desktop_type.id }}-time").innerHTML = {{ app_name }}_{{ desktop_type.id }}_minutes + ":" + {{ app_name }}_{{ desktop_type.id }}_seconds;
-}, 1000);
+var bar = document.getElementById("{{ app_name }}-{{ desktop_type.id }}-bar");
+var reloaded = false;
+var progress = 0;
+
+// var x = setInterval(function () {
+//     {{ app_name }}_{{ desktop_type.id }}_timer = {{ app_name }}_{{ desktop_type.id }}_timer - 1;
+//     if ({{ app_name }}_{{ desktop_type.id }}_timer <= 0 && !reloaded) {
+//         reloaded = true;
+//         window.location.reload(1);
+//     }
+//     const {{ app_name }}_{{ desktop_type.id }}_minutes = (Math.max(Math.floor({{ app_name }}_{{ desktop_type.id }}_timer/60), 0)).toString()
+//     const {{ app_name }}_{{ desktop_type.id }}_seconds = (Math.max({{ app_name }}_{{ desktop_type.id }}_timer%60, 0)).toString().padStart(2, "0")
+//     document.getElementById("{{ app_name }}-{{ desktop_type.id }}-time").innerHTML = {{ app_name }}_{{ desktop_type.id }}_minutes + ":" + {{ app_name }}_{{ desktop_type.id }}_seconds;
+// }, 1000);
 
 async function poll_status(url, interval) {
-    while(true) {
+    while(!reloaded) {
         await new Promise(r => setTimeout(r, interval));
         fetch(url)
             .then(resp => resp.json())
             .then(vm_status => {
                 console.log(vm_status);
+                progress = (vm_status.status_progress > progress) ? vm_status.status_progress : Math.min(++progress, 100);
+                // console.log(progress);
+                bar.setAttribute("aria-valuenow", progress);
+                bar.setAttribute("style", "width: " + progress + "%");
+                bar.innerHTML = vm_status.status_message;
                 if (vm_status.status != "{{ VM_WAITING }}") {
                     reloaded = true;
-                    window.location.reload(1);
-                } else {
-                    var bar = document.getElementById(
-                        "{{ app_name }}-{{ desktop_type.id }}-bar");
-                    var progress = vm_status.status_progress;
-                    bar.setAttribute("aria-valuenow", progress);
-                    bar.setAttribute("style", "width: " + progress + "%");
-                    bar.innerHTML = vm_status.status_message;
+                    window.setTimeout(function() {
+                        console.log("complete");
+                        window.location.reload(1);
+                    }, 1500);
                 }
             });
     }
