4213119848d94970dc45ce3ac252434b8d57b0be
Andy Botting <andy@andybotting.com>
2021-12-03 20:48:06 +1100

    Collect and compress now handled at container build time
    Change-Id: I20b6f7420f0810091bf9187e2b1fa2039836dbc4

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -6,15 +6,3 @@ if [ -n "$DJANGO_MIGRATE" ]; then
 else
   echo "** Skipping Django migrate **"
 fi
-
-if [ -n "$DJANGO_COLLECT_STATIC" ]; then
-  echo "** Starting Django collectstatic **"
-  django-admin collectstatic --noinput
-  echo "** Completed Django collectstatic **"
-
-  echo "** Starting Django compress **"
-  django-admin compress --force
-  echo "** Completed Django collectstatic **"
-else
-  echo "** Skipping Django collectstatic/compress **"
-fi
