f5e68c5d368d7f5a2525c4e1114047d2602cc728
Andy Botting <andy@andybotting.com>
2022-04-06 16:36:36 +1000

    Add the App static files finder back as the admin site needs it
    Change-Id: I7acc1a1a2441198701433d59851ba30e7f73c676

--- researcher_workspace/settings.py
+++ researcher_workspace/settings.py
@@ -183,6 +183,7 @@ USE_TZ = True
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 )
 
