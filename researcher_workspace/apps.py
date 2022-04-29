1bdda78ecc9fc668194b97a390793e39fbb22170
Andy Botting <andy@andybotting.com>
2022-04-29 11:26:57 +1000

    Add metrics
    Change-Id: I32ee50c0fa7446d936965d7432e77c68589575aa

--- researcher_workspace/apps.py
+++ researcher_workspace/apps.py
@@ -1,8 +1,37 @@
 from django.apps import AppConfig
 
+from django.db.models.signals import post_migrate
+from django.db.backends.signals import connection_created
+from prometheus_client import REGISTRY
+
+
+migration_executed = False
+monitoring_initialised = False
+
+
+def post_migration_callback(sender, **kwargs):
+    global migration_executed
+    migration_executed = True
+
+
+def connection_callback(sender, connection, **kwargs):
+    global monitoring_initialised
+    # Check to see if we are not running a unittest temp db
+    if not connection.settings_dict['NAME'] == 'file:memorydb_default?mode=memory&cache=shared':
+        if not monitoring_initialised:
+            from researcher_workspace import metrics
+            REGISTRY.register(metrics.BumblebeeMetricsCollector())
+            monitoring_initialised = True
+
 
 class ResearcherWorkspaceConfig(AppConfig):
     name = 'researcher_workspace'
 
     def ready(self):
         import researcher_workspace.signals  # noqa
+
+        global migration_executed
+        post_migrate.connect(post_migration_callback, sender=self)
+
+        if not migration_executed:
+            connection_created.connect(connection_callback)
---
+++ researcher_workspace/metrics.py
@@ -0,0 +1,54 @@
+from django.db.models import Count, EmailField, ExpressionWrapper, F, Func, Value
+
+from prometheus_client import Counter, Histogram, Gauge
+from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGISTRY
+
+from vm_manager import models as vm_manager_models
+
+
+class BumblebeeMetricsCollector(object):
+    def collect(self):
+        metrics = {}
+        volumes = vm_manager_models.Volume.objects.all()
+
+        for dimension in ['created', 'running', 'shelved']:
+
+            name = f"bumblebee_desktops_{dimension}"
+            desc = f"Number of {dimension} desktops"
+            if dimension == 'created':
+                qs = volumes
+            elif dimension == 'running':
+                qs = volumes.filter(deleted=None, shelved_at=None)
+            elif dimension == 'shelved':
+                qs = volumes.filter(deleted=None).exclude(shelved_at=None)
+
+            # bumblebee_desktops_*_total
+            yield GaugeMetricFamily(f"{name}_total", desc, value=qs.count())
+
+            # bumblebee_desktops_*_by_type
+            g = GaugeMetricFamily(
+                f"{name}_by_type", f"{desc} by_type", labels=['type'])
+            new_qs = qs.values('operating_system').annotate(Count('operating_system'))
+            for t in new_qs:
+                g.add_metric([t['operating_system']], t['operating_system__count'])
+            yield g
+
+            # bumblebee_desktops_*_by_zone
+            g = GaugeMetricFamily(
+                f"{name}_by_zone", f"{desc} by_zone", labels=['zone'])
+            new_qs = qs.values('zone').annotate(Count('zone'))
+            for t in new_qs:
+                g.add_metric([t['zone']], t['zone__count'])
+            yield g
+
+            # bumblebee_desktops_*_by_domain
+            g = GaugeMetricFamily(
+                f"{name}_by_domain", f"{desc} by domain", labels=['domain'])
+            new_qs = qs.values(
+                domain=ExpressionWrapper(
+                    Func(F('user__email'), Value('@'),
+                         Value(-1), function='substring_index'),
+                output_field=EmailField())).annotate(Count('domain'))
+            for t in new_qs:
+                g.add_metric([t['domain']], t['domain__count'])
+            yield g
