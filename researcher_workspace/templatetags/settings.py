60f05eef5b916437c6869d4c5e2fa7b92e3ba78c
Stephen Crawley <s.crawley@uq.edu.au>
2022-09-05 16:23:56 +1000

    Fix minor regression from removing BOOST lifetime
    It was displaying "for up to -1 days" nonsense.
    
    Change-Id: I577d97356f05b84836b9d58be43164775e1b26cf

--- researcher_workspace/templatetags/settings.py
+++ researcher_workspace/templatetags/settings.py
@@ -6,4 +6,17 @@ register = template.Library()
 
 @register.simple_tag
 def get_setting(name):
+    '''Return the value of a setting from Django's conf.settings.
+    '''
+
     return getattr(settings, name, '')
+
+
+@register.simple_tag(takes_context=True)
+def add_setting(context, setting_name, context_name):
+    '''Add the value of a setting from Django's conf.settings to
+    the template context.  Returns an empty string.
+    '''
+
+    context[context_name] = getattr(settings, setting_name, '')
+    return ''
