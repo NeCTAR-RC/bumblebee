c8af6a397fdc565dba1b361d7bb6f38000f09ef2
Stephen Crawley <s.crawley@uq.edu.au>
2023-03-09 11:54:36 +1100

    Fixed the VM_Shutdown page buttons
       - The confirmation modal didn't launch
       - The "Start" button in the model didn't work
    
    Change-Id: Id56298b29dd949579bf4d4be875190798015d842

--- vm_manager/templates/vm_manager/javascript/VM_Shutdown.js
+++ vm_manager/templates/vm_manager/javascript/VM_Shutdown.js
@@ -1,5 +1,5 @@
 var beenClicked = false;
-function reboot_{{ app_name }}_{{ desktop_type.id.capitalize }}(tag) {
+function reboot_{{ app_name }}_{{ desktop_type.id }}(tag) {
     if (!beenClicked) {
         beenClicked = true;
         tag.disabled = true;
